ba9af8a512376c02c5c5885bf8a912ab3a1f55bc
Nikhil Choudhary <nc1336282@gmail.com>
2021-03-13 15:34:04 +0530

    modified notification success message

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -85,7 +85,7 @@ const ContactForm = () => {
     notification = {
       status: "success",
       title: "Success!",
-      message: "Message sent successfully!",
+      message: "Message sent successfully!!!!!",
     };
   }
 
